4f21a166114a82ed70901aea9bab06600763ff5d
Chase J. Shyu <iamhemry@gmail.com>
2025-05-21 12:46:46 -0500

    remove map and vec2D in nearest_neighbor_interpolation()

--- nn-interpolation.cxx
+++ nn-interpolation.cxx
@@ -1,5 +1,4 @@
 #include "iostream"
-#include <map>
 #include <numeric>
 #include <stdexcept>
 #ifdef USE_NPROF
@@ -52,8 +51,9 @@ namespace {
                               int_vec &is_changed,
                               KDTree &kdtree,
                               int old_nelem,
-                              int_vec2D &elems_vec,
-                              double_vec2D &ratios_vec)
+                              int_vec &elems_vec,
+                              double_vec &ratios_vec,
+                              int_vec &idx_changed)
     {
 #ifdef USE_NPROF
         nvtxRangePushA(__FUNCTION__);
@@ -68,7 +68,7 @@ namespace {
         const double eps = 0;
 
         int nelem_changed = 0;
-        int_vec idx_changed(var.nelem);
+
         for (int e=0; e<var.nelem; e++) {
             if (is_changed[e]) {
                 idx_changed[e] = nelem_changed;
@@ -76,23 +76,21 @@ namespace {
             }
         }
 
-        elems_vec.resize(nelem_changed);
-        ratios_vec.resize(nelem_changed);
+        elems_vec.resize(nelem_changed*32,-1);
+        ratios_vec.resize(nelem_changed*32);
 
-        #pragma omp parallel for default(none) shared(var, bary, is_changed, kdtree, elems_vec, ratios_vec, idx_changed, max_el) 
+        #pragma omp parallel for default(none) shared(var, bary, is_changed, kdtree, elems_vec, ratios_vec, idx_changed, max_el)
         for(int e=0; e<var.nelem; e++) {
             if (is_changed[e]) {
-                std::map<int, int> elem_count;
-
+                int elem_count_buf[32] = {0};
+                int elem_keys[32] = {0};
+                int elem_size = 0;
                 /* Procedure:
                  *   1. Create a bunch of temporary points, uniformly distributed in the element.
                  *   2. Locate these points on old elements.
                  *   3. The percentage of points in each old elements is used as (approximate)
                  *      volume weighting for the mapping.
                  */
-
-                // std::cout << "Element " << e << " is changed:\n";
-
                 const int* conn = (*var.connectivity)[e];
                 for (int i=0; i<neta0; i++)
                     for (int j=0; j<neta1; j++) {
@@ -134,11 +132,18 @@ namespace {
                                 old_e = nn_idx[jj];
                                 bary.transform(x, old_e, r);
                                 if (bary.is_inside(r)) {
-                                    try {
-                                        ++ elem_count.at(old_e);
+                                    bool found = false;
+                                    for (int ei = 0; ei < elem_size; ++ei) {
+                                        if (elem_keys[ei] == old_e) {
+                                            elem_count_buf[ei]++;
+                                            found = true;
+                                            break;
+                                        }
                                     }
-                                    catch (std::out_of_range const &exc) {
-                                        elem_count[old_e] = 1;
+                                    if (!found && elem_size < 32) {
+                                        elem_keys[elem_size] = old_e;
+                                        elem_count_buf[elem_size] = 1;
+                                        elem_size++;
                                     }
                                     break;
                                 }
@@ -147,7 +152,6 @@ namespace {
                             /* not found, do nothing */
                             // std::cout << " not found\n";
                             continue;
-
 #ifdef THREED
                         }
 #endif
@@ -155,8 +159,8 @@ namespace {
 
                 // Count
                 int total_count = 0;
-                for (auto i=elem_count.begin(); i!=elem_count.end(); ++i)
-                    total_count += i->second;
+                for (int i=0; i<=elem_size; ++i)
+                    total_count += elem_count_buf[i];
 
                 // std::cout << "  has " << total_count << " points\n";
 
@@ -165,45 +169,25 @@ namespace {
                     // and this element is completely within the new material.
                     // Mark the element as unchanged instead to keep the result
                     // of nearest neighbor interpolation.
-                    is_changed[e] = 0;
+                    is_changed[e] = -1;
                     continue;
                 }
 
-                if (elem_count.size() == 1) {
+                if (elem_size == 1) {
                     // This happens when the new is completely within the old element.
                     // Mark the element as unchanged instead to keep the result
                     // of nearest neighbor interpolation.
-                    is_changed[e] = 0;
-                    elem_count.clear();
+                    is_changed[e] = -1;
                     continue;
                 }
 
-                int_vec &elems = elems_vec[idx_changed[e]];
-                elems.reserve(elem_count.size());
-
-                double_vec &ratios = ratios_vec[idx_changed[e]];
-                ratios.reserve(elem_count.size());
-
                 const double inv = 1.0 / total_count;
-                for (auto i=elem_count.begin(); i!=elem_count.end(); ++i) {
-                    elems.push_back(i->first);
-                    ratios.push_back(i->second * inv);
+                for (int i=0; i<elem_size; ++i) {
+                    elems_vec[idx_changed[e]*32+i] = elem_keys[i];
+                    ratios_vec[idx_changed[e]*32+i] = elem_count_buf[i] * inv;
                 }
-
             }
         }
-
-    elems_vec.erase(
-        std::remove_if(elems_vec.begin(), elems_vec.end(),
-                    [](const int_vec& v) { return v.empty(); }),
-        elems_vec.end()
-    );
-
-    ratios_vec.erase(
-        std::remove_if(ratios_vec.begin(), ratios_vec.end(),
-                    [](const double_vec& v) { return v.empty(); }),
-        ratios_vec.end()
-    );
 
 #ifdef USE_NPROF
         nvtxRangePop();
@@ -217,8 +201,9 @@ namespace {
                                const conn_t &old_connectivity,
                                int_vec &idx,
                                int_vec &is_changed,
-                               int_vec2D &elems_vec,
-                               double_vec2D &ratios_vec)
+                               int_vec &idx_changed,
+                               int_vec &elems_vec,
+                               double_vec &ratios_vec)
     {
 #ifdef USE_NPROF
         nvtxRangePushA(__FUNCTION__);
@@ -238,7 +223,7 @@ namespace {
 
         find_nearest_neighbor(var, kdtree, idx, is_changed);
 
-        find_acm_elem_ratios(var, bary, is_changed, kdtree, old_nelem, elems_vec, ratios_vec);
+        find_acm_elem_ratios(var, bary, is_changed, kdtree, old_nelem, elems_vec, ratios_vec, idx_changed);
 
         delete old_center;
 #ifdef USE_NPROF
@@ -249,73 +234,90 @@ namespace {
 
     void inject_field(const int_vec &idx,
                       const int_vec &is_changed,
-                      const int_vec2D &elems_vec,
-                      const double_vec2D &ratios_vec,
+                      const int_vec &idx_changed,
+                      const int_vec &elems_vec,
+                      const double_vec &ratios_vec,
                       const double_vec &source,
                       double_vec &target)
     {
-        #pragma omp parallel for default(none)          \
-            shared(idx, source, target)
-        for (std::size_t i=0; i<target.size(); i++) {
-            int n = idx[i];
-            target[i] = source[n];
-        }
+#ifdef USE_NPROF
+        nvtxRangePushA(__FUNCTION__);
+#endif
+        #pragma omp parallel default(none) shared(idx, source, target, is_changed, idx_changed)
+        {
+            #pragma omp for
+            for (std::size_t i=0; i<target.size(); i++) {
+                int n = idx[i];
+                target[i] = source[n];
+            }
 
-        int n = 0;
-        for (std::size_t i=0; i<target.size(); i++) {
-            if (is_changed[i]) {
-                const int_vec &elems = elems_vec[n];
-                const double_vec &ratios = ratios_vec[n];
+            #pragma omp for
+            for (std::size_t i=0; i<target.size(); i++) {
+                if (is_changed[i]>0) {
+                    int n = idx_changed[i];
 
-                target[i] = 0;
-                for (std::size_t j=0; j<elems.size(); j++) {
-                    target[i] += ratios[j] * source[ elems[j] ];
+                    target[i] = 0;
+                    for (std::size_t j=0; j<32; j++) {
+                        if (elems_vec[n*32+j] < 0) break;
+                        target[i] += ratios_vec[n*32+j] * source[ elems_vec[n*32+j] ];
+                    }
                 }
-                n ++;
             }
         }
+#ifdef USE_NPROF
+        nvtxRangePop();
+#endif
     }
 
 
     void inject_field(const int_vec &idx,
                       const int_vec &is_changed,
-                      const int_vec2D &elems_vec,
-                      const double_vec2D &ratios_vec,
+                      const int_vec &idx_changed,
+                      const int_vec &elems_vec,
+                      const double_vec &ratios_vec,
                       const tensor_t &source,
                       tensor_t &target)
     {
-        #pragma omp parallel for default(none)          \
-            shared(idx, source, target)
-        for (std::size_t i=0; i<target.size(); i++) {
-            int n = idx[i];
-            for (int d=0; d<NSTR; d++) {
-                target[i][d] = source[n][d];
-            }
-        }
-
-        int n = 0;
-        for (std::size_t i=0; i<target.size(); i++) {
-            if (is_changed[i]) {
-                const int_vec &elems = elems_vec[n];
-                const double_vec &ratios = ratios_vec[n];
-
+#ifdef USE_NPROF
+        nvtxRangePushA(__FUNCTION__);
+#endif
+        #pragma omp parallel default(none) shared(idx, source, target, is_changed, idx_changed)
+        {
+            #pragma omp for
+            for (std::size_t i=0; i<target.size(); i++) {
+                int n = idx[i];
                 for (int d=0; d<NSTR; d++) {
-                    target[i][d] = 0;
-                    for (std::size_t j=0; j<elems.size(); j++) {
-                        target[i][d] += ratios[j] * source[ elems[j] ][d];
+                    target[i][d] = source[n][d];
+                }
+            }
+
+            #pragma omp for
+            for (std::size_t i=0; i<target.size(); i++) {
+                if (is_changed[i]>0) {
+                    int n = idx_changed[i];
+
+                    for (int d=0; d<NSTR; d++) {
+                        target[i][d] = 0;
+                        for (std::size_t j=0; j<32; j++) {
+                            if (elems_vec[n*32+j] < 0) break;
+                            target[i][d] += ratios_vec[n*32+j] * source[ elems_vec[n*32+j] ][d];
+                        }
                     }
                 }
-                n ++;
             }
         }
+#ifdef USE_NPROF
+        nvtxRangePop();
+#endif
     }
 
 
     void nn_interpolate_elem_fields(Variables &var,
                                     const int_vec &idx,
                                     const int_vec &is_changed,
-                                    const int_vec2D &elems_vec,
-                                    const double_vec2D &ratios_vec)
+                                    const int_vec &idx_changed,
+                                    const int_vec &elems_vec,
+                                    const double_vec &ratios_vec)
     {
 #ifdef USE_NPROF
         nvtxRangePushA(__FUNCTION__);
@@ -326,33 +328,33 @@ namespace {
         double_vec *a;
 
         a = new double_vec(e);
-        inject_field(idx, is_changed, elems_vec, ratios_vec, *var.plstrain, *a);
+        inject_field(idx, is_changed, idx_changed, elems_vec, ratios_vec, *var.plstrain, *a);
         delete var.plstrain;
         var.plstrain = a;
 
         a = new double_vec(e);
-        inject_field(idx, is_changed, elems_vec, ratios_vec, *var.delta_plstrain, *a);
+        inject_field(idx, is_changed, idx_changed, elems_vec, ratios_vec, *var.delta_plstrain, *a);
         delete var.delta_plstrain;
         var.delta_plstrain = a;
 
         tensor_t *b;
         b = new tensor_t(e);
-        inject_field(idx, is_changed, elems_vec, ratios_vec, *var.strain, *b);
+        inject_field(idx, is_changed, idx_changed, elems_vec, ratios_vec, *var.strain, *b);
         delete var.strain;
         var.strain = b;
 
         b = new tensor_t(e);
-        inject_field(idx, is_changed, elems_vec, ratios_vec, *var.stress, *b);
+        inject_field(idx, is_changed, idx_changed, elems_vec, ratios_vec, *var.stress, *b);
         delete var.stress;
         var.stress = b;
 
         a = new double_vec(e);
-        inject_field(idx, is_changed, elems_vec, ratios_vec, *var.stressyy, *a);
+        inject_field(idx, is_changed, idx_changed, elems_vec, ratios_vec, *var.stressyy, *a);
         delete var.stressyy;
         var.stressyy = a;
 
         a = new double_vec(e);
-        inject_field(idx, is_changed, elems_vec, ratios_vec, *var.old_mean_stress, *a);
+        inject_field(idx, is_changed, idx_changed, elems_vec, ratios_vec, *var.old_mean_stress, *a);
         delete var.old_mean_stress;
         var.old_mean_stress = a;
 
@@ -362,12 +364,12 @@ namespace {
         // var.stress_old = b;
 
         a = new double_vec(e);
-        inject_field(idx, is_changed, elems_vec, ratios_vec, *var.radiogenic_source, *a);
+        inject_field(idx, is_changed, idx_changed, elems_vec, ratios_vec, *var.radiogenic_source, *a);
         delete var.radiogenic_source;
         var.radiogenic_source = a;
 
         a = new double_vec(e);
-        inject_field(idx, is_changed, elems_vec, ratios_vec, *var.surfinfo.edvacc_surf, *a);
+        inject_field(idx, is_changed, idx_changed, elems_vec, ratios_vec, *var.surfinfo.edvacc_surf, *a);
         delete var.surfinfo.edvacc_surf;
         var.surfinfo.edvacc_surf = a;
 
@@ -395,16 +397,15 @@ void nearest_neighbor_interpolation(Variables &var,
 #endif
     int_vec idx(var.nelem); // nearest element
     int_vec is_changed(var.nelem); // is the element changed during remeshing?
+    int_vec idx_changed(var.nelem); 
 
-    int_vec2D elems_vec;
-    double_vec2D ratios_vec;
+    int_vec elems_vec;
+    double_vec ratios_vec;
 
-    prepare_interpolation(var, bary, old_coord, old_connectivity, idx, is_changed, elems_vec, ratios_vec);
+    prepare_interpolation(var, bary, old_coord, old_connectivity, idx, is_changed, idx_changed, elems_vec, ratios_vec);
 
-    // print(std::cout, elems_vec);
-    // print(std::cout, ratios_vec);
+    nn_interpolate_elem_fields(var, idx, is_changed, idx_changed, elems_vec, ratios_vec);
 
-    nn_interpolate_elem_fields(var, idx, is_changed, elems_vec, ratios_vec);
 #ifdef USE_NPROF
     nvtxRangePop();
 #endif
