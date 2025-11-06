666b779f6e0ff8f93e14d65bf9e4339c54a92110
Chase J. Shyu <iamhemry@gmail.com>
2025-11-06 09:04:46 -0600

    update: update variable description for show_information_interval_in_sec

--- input.cxx
+++ input.cxx
@@ -43,8 +43,9 @@ static void declare_parameters(po::options_description &cfg,
         ("sim.output_time_interval_in_yr", po::value<double>(&p.sim.output_time_interval_in_yr),
          "Output time interval (in years)")
         ("sim.show_information_interval_in_sec", po::value<int>(&p.sim.show_information_interval_in_sec)->default_value(0),
-         "Output time interval for showing simulation information on screen (in seconds). \n"
-         "0: no time-based output on screen")
+         "Output walltime interval for showing model status on screen (in seconds).\n"
+         " 0: no printing model status on screen based on walltime intervel.\n"
+         ">0: specified second interval for showing model status on screen after the last result output.")
 
         ("sim.checkpoint_frame_interval", po::value<int>(&p.sim.checkpoint_frame_interval)->default_value(10),
          "How frequent to write checkpoint file (used for restarting simulation)?")
