5e7b64224cce66af1afb93be80b130a2ac265929
sungho91 <sungho91@snu.ac.kr>
2026-09-17 08:46:49 +0900

    refactor(rsf): reuse the existing element shape gradients
    Move get_local_shape_fn from fields.cxx to fields.hpp with its existing
    signatures and arithmetic intact. Use that shared inline routine for the
    current-rate timestep bound and remove get_geometry_shape_fn. Keep OpenACC
    sequential device-call support on the shared routine.
    
    Validation: clean 2D/3D OpenMP/HDF5 and debug builds; RSF controls and
    restart checks with 1 and 4 threads; all nine simple-shear cases plus
    healing; NVHPC compilation of the affected 2D/3D OpenACC objects. Short
    before/after EP, EVP and localization runs match all 1,174 scientific
    dataset instances exactly, excluding only wall-clock timing metadata.
    GPU execution and the full localization campaigns were not rerun.

--- geometry.cxx
+++ geometry.cxx
@@ -8,72 +8,10 @@
 #include "matprops.hpp"
 #include "utils.hpp"
 #include "geometry.hpp"
+#include "fields.hpp"
 #include "bc.hpp"
 #include "mesh.hpp"
 #include "output.hpp"
-
-#ifdef THREED
-#pragma acc routine seq
-static inline void get_geometry_shape_fn(
-    const Variables &var, int e,
-    double shpdx[4], double shpdy[4], double shpdz[4])
-{
-    ConstArrayIndirectAccessor d =
-        var.coord->view_const((*var.connectivity)[e]);
-    const double iv = 1.0 / (6.0 * (*var.volume)[e]);
-
-    const double x01 = d[0][0] - d[1][0];
-    const double x02 = d[0][0] - d[2][0];
-    const double x03 = d[0][0] - d[3][0];
-    const double x12 = d[1][0] - d[2][0];
-    const double x13 = d[1][0] - d[3][0];
-    const double x23 = d[2][0] - d[3][0];
-    const double y01 = d[0][1] - d[1][1];
-    const double y02 = d[0][1] - d[2][1];
-    const double y03 = d[0][1] - d[3][1];
-    const double y12 = d[1][1] - d[2][1];
-    const double y13 = d[1][1] - d[3][1];
-    const double y23 = d[2][1] - d[3][1];
-    const double z01 = d[0][2] - d[1][2];
-    const double z02 = d[0][2] - d[2][2];
-    const double z03 = d[0][2] - d[3][2];
-    const double z12 = d[1][2] - d[2][2];
-    const double z13 = d[1][2] - d[3][2];
-    const double z23 = d[2][2] - d[3][2];
-
-    shpdx[0] = iv * (y13*z12 - y12*z13);
-    shpdx[1] = iv * (y02*z23 - y23*z02);
-    shpdx[2] = iv * (y13*z03 - y03*z13);
-    shpdx[3] = iv * (y01*z02 - y02*z01);
-
-    shpdy[0] = iv * (z13*x12 - z12*x13);
-    shpdy[1] = iv * (z02*x23 - z23*x02);
-    shpdy[2] = iv * (z13*x03 - z03*x13);
-    shpdy[3] = iv * (z01*x02 - z02*x01);
-
-    shpdz[0] = iv * (x13*y12 - x12*y13);
-    shpdz[1] = iv * (x02*y23 - x23*y02);
-    shpdz[2] = iv * (x13*y03 - x03*y13);
-    shpdz[3] = iv * (x01*y02 - x02*y01);
-}
-#else
-#pragma acc routine seq
-static inline void get_geometry_shape_fn(
-    const Variables &var, int e, double shpdx[3], double shpdz[3])
-{
-    ConstArrayIndirectAccessor d =
-        var.coord->view_const((*var.connectivity)[e]);
-    const double iv = 1.0 / (2.0 * (*var.volume)[e]);
-
-    shpdx[0] = iv * (d[1][1] - d[2][1]);
-    shpdx[1] = iv * (d[2][1] - d[0][1]);
-    shpdx[2] = iv * (d[0][1] - d[1][1]);
-
-    shpdz[0] = iv * (d[2][0] - d[1][0]);
-    shpdz[1] = iv * (d[0][0] - d[2][0]);
-    shpdz[2] = iv * (d[1][0] - d[0][0]);
-}
-#endif
 
 #pragma acc routine seq
 static inline double current_strain_rate_invariant(
@@ -83,11 +21,11 @@ static inline double current_strain_rate_invariant(
     double shpdx[NODES_PER_ELEM];
     double shpdy[NODES_PER_ELEM];
     double shpdz[NODES_PER_ELEM];
-    get_geometry_shape_fn(var, e, shpdx, shpdy, shpdz);
+    get_local_shape_fn(var, e, shpdx, shpdy, shpdz);
 #else
     double shpdx[NODES_PER_ELEM];
     double shpdz[NODES_PER_ELEM];
-    get_geometry_shape_fn(var, e, shpdx, shpdz);
+    get_local_shape_fn(var, e, shpdx, shpdz);
 #endif
     ConstArrayIndirectAccessor v =
         var.vel->view_const((*var.connectivity)[e]);
